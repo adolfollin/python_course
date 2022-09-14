57d567801378b0049053daf04332ef2bdf76ab63
adolfollin <adolfollin>
2022-09-14 01:20:57 +0000

    test

---
+++ Ejercicios/ej2.py
@@ -0,0 +1,5 @@
+# Databricks notebook source
+import numpy as np
+print(np.array([range(3, 29)]))
+print('Arreglo: ', np.array([0,1,2,3,4,0,1,2,3,4]))
+print('Arreglo ordenado de forma ascendente: ', np.sort(np.array([0,1,2,3,4,0,1,2,3,4])))
