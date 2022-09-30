ee3be056f4ba2bd2e81329e7093d7a2418f04d85
adolfollin <adolfollin>
2022-09-30 02:20:37 +0000

    ej2

--- Ejercicios/ej3.py
+++ Ejercicios/ej3.py
@@ -1,6 +1,5 @@
 # Databricks notebook source
-import numpy as np
-
-# COMMAND ----------
-
-
+tupla = (1,'hola', True, None, 10.5)
+lista_de_tupla = list(tupla)
+diccionario = dict(zip(range(1,6), lista_de_tupla))
+print(diccionario)
